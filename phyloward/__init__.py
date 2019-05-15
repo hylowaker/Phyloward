e8aa16d2462b5f36c7497478240209b19f662450
jH <jaeheung.han@data-alliance.com>
2019-05-15 11:05:04 +0900

    fix no domain hit error

--- phyloward/__init__.py
+++ phyloward/__init__.py
@@ -1,8 +1,8 @@
 __author__ = 'JaeHeung Han'
-__copyright__ = 'Copyright (c) 2018 JaeHeung Han'
+__copyright__ = 'Copyright (c) 2018-2019 JaeHeung Han'
 __license__ = 'GPLv3'
 __email__ = 'hylowaker@gmail.com'
-__version__ = '0.4.0.dev0'
+__version__ = '0.4.0.dev1'
 __status__ = 'Development'
 
 __all__ = ['extractor', 'aligner', '__version__']
