2abdd5d00955ab3e844c99c384f29cfce7e33588
jH <jaeheung.han@data-alliance.com>
2019-05-21 16:27:04 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
 
